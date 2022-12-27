b6e971391b5d4c5c53ef2522a7310001b4a69f28
George Beshay <90455303+GeorgeBeshay@users.noreply.github.com>
2022-12-28 00:59:33 +0200

    Working on the mail box interface

--- Front End/Mail_WebApplication_FE/src/app/Components/mail-page/mail-page.component.ts
+++ Front End/Mail_WebApplication_FE/src/app/Components/mail-page/mail-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BackEndCallerService } from 'src/app/Services/back-end-caller.service';
+import { Email } from 'src/app/Interfaces/email';
+import { User } from 'src/app/Interfaces/user';
+import { Folder } from 'src/app/Interfaces/folder';
 
 @Component({
   selector: 'app-mail-page',
@@ -9,11 +12,53 @@ import { BackEndCallerService } from 'src/app/Services/back-end-caller.service';
 })
 export class MailPageComponent implements OnInit {
   private myBECaller: BackEndCallerService;
+  public selectedFolder: string = '';
+  public selectedEmail!: Email;
   // -------------- Separator --------------
   constructor(private http: HttpClient) {
     this.myBECaller = new BackEndCallerService(this.http);
   }
   // -------------- Separator --------------
-
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.resetSelectedEmail();
+  }
+  // -------------- Separator --------------
+  myEmails: Email[] = [];
+  generateMails() {
+    this.myEmails.push({
+      sender: 'UserA',
+      receiver: 'UserB',
+      subject: 'Party Invitation',
+      body: "Hey UserB, I'd lik to invite you to my birthday party next tuesday morning at 12:00 AM, kindly check the inviiation card at the attachments section.",
+      attachments: [{ Name: 'Invitation Card', 'Card Number': '#22342342' }],
+    });
+    this.myEmails.push({
+      sender: 'UserX',
+      receiver: 'UserB',
+      subject: 'Wedding Invitation',
+      body: "Hey UserB, I'd lik to invite you to my wedding party next thursday morning at 12:00 AM, kindly check the inviiation card at the attachments section.",
+      attachments: [{ Name: 'Invitation Card', 'Card Number': '#654651' }],
+    });
+  }
+  // -------------- Separator --------------
+  selectFolder(folderName: string) {
+    this.selectedFolder = folderName;
+    this.generateMails();
+    this.resetSelectedEmail();
+  }
+  selectEmail(emailSubject: Email) {
+    this.selectedEmail = emailSubject;
+  }
+  refreshMailBox() {}
+  // -------------- Separator --------------
+  resetSelectedEmail() {
+    this.selectedEmail = {
+      sender: 'NA',
+      receiver: 'NA',
+      subject: 'NA',
+      body: 'NA',
+      attachments: [],
+    };
+  }
+  // -------------- Separator --------------
 }
--- Front End/Mail_WebApplication_FE/src/app/Interfaces/email.ts
+++ Front End/Mail_WebApplication_FE/src/app/Interfaces/email.ts
@@ -2,5 +2,6 @@ export interface Email {
   sender: string;
   receiver: string;
   subject: string;
+  body: string;
   attachments: Object[];
 }
